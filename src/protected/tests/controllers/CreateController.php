e247cdbc10b269781b969e7c5baae7bb1243e3da
Christian Micklisch <cmicklis@stetson.edu>
2017-05-26 16:31:38 -0400

    Tested The Creation of an asset

--- src/protected/tests/controllers/CreateController.php
+++ src/protected/tests/controllers/CreateController.php
@@ -6,6 +6,8 @@
  * @author  Christian Micklisch <christian.micklisch@successwithsos.com>
  */
 
+use Common\Reflection;
+
 /**
  * CreateController_Test class. A PHPUnit Test case class.
  *
@@ -29,6 +31,30 @@ class CreateController_Test extends TestController
      *
      *
      *
+     * Input
+     *
+     *
+     *
+     */
+
+    /**
+     * Contains an array of test files for creating.
+     * 
+     * @return array An array of files
+     */
+    public function input_actionAssetCreate()
+    {
+        return [
+            [
+                TestController::COMPARISON_DIRECTORY . '/contain_aspect_true.png'
+            ]
+        ];
+    }
+
+    /**
+     *
+     *
+     *
      * Test
      *
      *
@@ -45,5 +71,38 @@ class CreateController_Test extends TestController
             '{"errors":{"general":["Not a proper http method type, please send a FILE"]}}';
 
         $this->assertControllerResponse('actionIndex',  '/create/', $expected_output);
+    }
+
+    /**
+     * Adds a file to the $_FILE variable and calls the create controller
+     * 
+     * @dataProvider input_actionAssetCreate
+     */
+    public function test_actionAssetCreate($file_path = "")
+    {
+        
+        $_FILES = [
+            'file' => [
+                'tmp_name' => $file_path,
+                'name' => 'Hello'
+            ]
+        ];
+
+        $expected_output = "HTTP/1.1 200 OK\n" .
+                "Content-type: application/json\n" .
+                '{"success":"Asset will be deleted."}';
+
+        $_SERVER['REDIRECT_URL'] = '/create/';
+        ob_start();
+        $controller = new $this->controller_name(rand(0,1000));
+        Reflection::setProperty('generateHeader', $this->controller_name, $controller, false);
+        Reflection::callMethod('actionIndex', $this->controller_name, [], $controller);
+        $response = ob_get_contents();
+        ob_end_clean();
+        
+        $json_response = str_replace("HTTP/1.1 200 OK\n", "", $response);
+        $json_response = str_replace("Content-type: application/json\n", "", $json_response);
+        $this->assertTrue(Asset::model()->fileName(json_decode($json_response)->public_url)->exists());
+
     }
 }
