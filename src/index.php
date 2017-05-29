530217a901d98c73e8ec9cb50f0935779db36e65
Christian Micklisch <cmicklis@stetson.edu>
2017-05-29 00:29:08 -0400

    Updated the index in php to include the file exception files

--- src/index.php
+++ src/index.php
@@ -19,6 +19,7 @@ error_reporting(-1);
 $files = array_merge(
     glob(dirname(__FILE__) . '/protected/vendor/fufu70/reflection-class/src/*.php'),
     glob(dirname(__FILE__) . '/protected/vendor/fufu70/file-class/src/*.php'),
+    glob(dirname(__FILE__) . '/protected/vendor/fufu70/file-class/src/file/*.php'),
     glob(dirname(__FILE__) . '/protected/vendor/milf/common-php/src/*.php'),
     glob(dirname(__FILE__) . '/protected/vendor/milf/asset-library/src/*.php'),
     glob(dirname(__FILE__) . '/protected/vendor/milf/asset-library/src/action/*.php'),
