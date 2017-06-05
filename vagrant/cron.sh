cbcb5fb86cd50fc03406c506e112ca790b4300d8
Christian Micklisch <cmicklis@stetson.edu>
2017-06-04 21:44:09 -0400

    on a small micro instance it is always a good idea to have a cron job to start the mysql service ... especially when the mysql service goes down

--- vagrant/cron.sh
+++ vagrant/cron.sh
@@ -5,4 +5,5 @@ sudo apt-get -y --force-yes install cron
 
 # add test console command to the cron tab
 
-crontab -l | { cat; echo "* * * * * sudo php /var/www/assetapi/protected/cron.php garbagecollection"; } | crontab -
+crontab -l | { cat; echo "* * * * * sudo php /var/www/asset_api/protected/cron.php garbagecollection"; } | crontab -
+crontab -l | { cat; echo "* * * * * run-one sudo service mysql start"; } | crontab -
